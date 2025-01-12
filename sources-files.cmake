9ada12b69eb3e239059db8516cac5338583367c0
mvaranda <m@varanda.ca>
2025-01-12 13:17:28 -0500

    reorg dir structure

---
+++ sources-files.cmake
@@ -0,0 +1,18 @@
+#
+# list of simulator source files
+#
+
+#
+# files part of the simulator framework
+#
+set(SIM_FILES 
+    src/sim/lvgl-sim.c
+    )
+
+
+#
+# portable Application files 
+#
+set(APP_FILES
+    src/app/app-ui.c
+    )
