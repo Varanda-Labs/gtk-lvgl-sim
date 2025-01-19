86ab019b30d59294c52c5451c39527a0d0a85984
Marcelo Varanda <Marcelo.Varanda@sbdinc.com>
2025-01-19 15:51:53 -0500

    add conditional build

--- mk_demo_music.sh
+++ mk_demo_music.sh
@@ -11,8 +11,8 @@ if [ ! -d "build" ]; then
   echo "creating build directory"
   mkdir build
   cd build
-  cmake ..
-  #cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ..
+  cmake -DMUSIC_DEMO=ON ..
+  #cmake -DMUSIC_DEMO=ON -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ..
 else
   cd build
 fi
